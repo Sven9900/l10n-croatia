f27e668e0d0a606c89d53836d89dfe7a5ce49df4
Davor Bojkić <bole@dajmi5.com>
2021-04-02 14:26:10 +0200

    l10n_hr - port to v14

---
+++ l10n_hr_base/__manifest__.py
@@ -0,0 +1,42 @@
+
+
+{
+    "name": """Croatia - base""",
+    "summary": """Croatia base localization data""",
+    "category": "Croatia",
+    "images": [],
+    "version": "12.0.1.0.0",
+    "application": False,
+
+    'author': "Decodio Application ltd., DAJ MI 5",
+    'website': "http://www.decod.io",
+    "support": "support@decod.io",
+    "licence": "LGPL-3",
+    #"price" : 20.00,   #-> only if module if sold!
+    #"currency": "EUR",
+
+    "depends": [
+
+    ],
+    "external_dependencies": {
+        "python": [
+            'tzlocal'
+        ],
+        "bin": []
+    },
+    "data": [
+
+        "views/res_company_view.xml",
+        "data/module_category_croatia.xml",
+        # moved to NKD
+        #"views/l10n_hr_nkd_view.xml",
+        #"data/localization_settings.xml",
+        #"security/ir.model.access.csv",
+    ],
+    "qweb": [],
+    "demo": [],
+
+    "auto_install": False,
+    "installable": True,
+}
+
