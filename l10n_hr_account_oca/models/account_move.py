317a2bfb145593fb50dce4a323bfd9ed59fea237
Davor Bojkić <bole@dajmi5.com>
2021-05-05 10:11:53 +0200

    activate premisse and sequence check

--- l10n_hr_account_oca/models/account_move.py
+++ l10n_hr_account_oca/models/account_move.py
@@ -51,12 +51,6 @@ class AccountMove(models.Model):
         domain="[('fiskal_responsible','=',True)]",
         help="Odgovorna osoba za ovaj račun",
         readonly=True, states={'draft': [('readonly', False)]})
-    # fiskal_user_id = fields.Many2one(
-    #     comodel_name='res.users',
-    #     string='Potvrdio',
-    #     readonly=True, states={'draft': [('readonly', False)]},
-    #     help='Fiskalizacija. Osoba koja je potvrdila racun',
-    #     copy=False)
 
     @api.model
     def default_get(self, fields):
@@ -98,8 +92,8 @@ class AccountMove(models.Model):
 
 
     def _post(self, soft=True):
-        res = super(AccountMove, self)._post(soft=soft)
-        for move in self:
+        posted = super()._post(soft=soft)
+        for move in posted:
             if not move.company_id.croatia:
                 continue  # only for croatia
             if not move.is_invoice(include_receipts=False):
@@ -110,16 +104,37 @@ class AccountMove(models.Model):
                         "business premisse %s is not active!" %
                         move.fiskal_uredjaj_id.prostor_id.name
                     ))
-            move.fiskalni_broj = move._gen_fiskal_number()
             if not move.date_document:
                 move.date_document = fields.Date.today()
             if not move.vrijeme_izdavanja:
                 datum = move.company_id.get_l10n_hr_time_formatted()
                 move.vrijeme_izdavanja = datum['datum_racun']
+
+            self.env.cr.execute("""
+            select name, fiskalni_broj, date from account_move
+            where journal_id = %(journal)s
+              and state = 'posted'
+              and date > %(date)s
+              --and vrijeme_izdavanja::timestamp > %(fiskal)s
+            order by date desc
+            """, {
+                'journal': self.journal_id.id,
+                'date': self.date,
+                'fiskal': datum['datum_racun']
+            })
+            res = self.env.cr.fetchall()
+            if res:
+                msg = _("Date %s is not valid!" % self.date)
+                for name, broj, dt in res:
+                    msg += "\n %s - %s from %s" %(name, broj, dt)
+                raise Warning(msg)
+
+            move.fiskalni_broj = move._gen_fiskal_number()
+
             # now and set lock on journals,
             # after first posting journal is locked for changes
             if not move.fiskal_uredjaj_id.lock:
                 move.fiskal_uredjaj_id.lock = True
             if not move.fiskal_uredjaj_id.prostor_id.lock:
                 move.fiskal_uredjaj_id.prostor_id.lock = True
-        return res
+        return posted
