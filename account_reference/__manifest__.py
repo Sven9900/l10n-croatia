6c437e477220cd1feaa009edcd8aa1978af4640e
Davor Bojkić <bole@dajmi5.com>
2021-05-06 09:14:46 +0200

    account refeence - poziv na broj - initial commit

---
+++ account_reference/__manifest__.py
@@ -0,0 +1,32 @@
+{
+    "name": """Account reference: Poziv na broj""",
+    "summary": """Add reference type: Poziv na broj""",
+    "category": "Accounting",
+    "images": [],
+    "version": "14.0.1.0.0",
+    "application": False,
+
+    "author": "DAJ MI 5, Decodio Applications ltd",
+    "licence": "LGPL-3",
+
+    "depends": [
+        'account'
+    ],
+    "external_dependencies": {
+        "python": [],
+        "bin": []
+    },
+    "data": [
+        "views/account_journal_view.xml",
+        "views/report_invoice.xml"
+    ],
+    "qweb": [],
+    "demo": [],
+
+    "post_load": None,
+    "pre_init_hook": None,
+    "post_init_hook": None,
+
+    "auto_install": False,
+    "installable": True,
+}
