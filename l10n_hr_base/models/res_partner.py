d1cc00f6b3fb8d565287aac610e9338f96696b02
Davor Bojkić - Bole <bole@dajmi5.com>
2022-11-28 09:53:06 +0100

    [IMP] get_oib funstionality native present in odoo16, functionality moved to l10n_hr Company notebook placeholder moved to l10n_hr

--- l10n_hr_base/models/res_partner.py
+++ l10n_hr_base/models/res_partner.py
@@ -3,11 +3,13 @@ from odoo import api, fields, models, _
 class Partner(models.Model):
     _inherit = 'res.partner'
 
-    def get_oib(self):
-        self.ensure_one()
-        if not self.vat:
-            # no vat - no error
-            return False
-        vat = self.vat.upper()  # in case someone entered in lowercase?
-        res = 'HR' in vat and vat.replace('HR', '') or vat
-        return res
+
+    # l10n_hr - def _compute_company_registry(self):
+    # def get_oib(self):
+    #     self.ensure_one()
+    #     if not self.vat:
+    #         # no vat - no error
+    #         return False
+    #     vat = self.vat.upper()  # in case someone entered in lowercase?
+    #     res = 'HR' in vat and vat.replace('HR', '') or vat
+    #     return res
